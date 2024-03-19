01caf81b00302b3e706a33f22a66faa989ccd19b
Ruslan Kabatsayev <b7.10110111@gmail.com>
2024-03-19 13:32:11 +0400

    Add a converter for sky cultures from old format to new
    It's still quite raw, especially regarding the conversion of
    descriptions and their translations, but at least it converts
    the other data.

---
+++ util/skyculture-converter/NamesOldLoader.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <QMap>
+#include <QString>
+
+class NamesOldLoader
+{
+	QMap<int, std::vector<QString>> starNames;
+	QMap<QString/*dsoId*/,std::vector<QString/*name*/>> dsoNames;
+	QMap<QString/*dsoId*/,std::vector<QString/*name*/>> planetNames;
+	QMap<QString/*dsoId*/,std::vector<QString/*name*/>> planetNamesMeaning;
+
+	void loadStarNames(const QString& skyCultureDir);
+	void loadDSONames(const QString& skyCultureDir);
+	void loadPlanetNames(const QString& skyCultureDir);
+public:
+	void load(const QString& skyCultureDir);
+	bool dumpJSON(std::ostream& s) const;
+};
