874913475d2911d83c1f4cf4c428d8aafbe6f3aa
Georg Zotti <Georg.Zotti@univie.ac.at>
2024-08-24 13:45:46 +0200

    Clearer warning messages

--- src/core/modules/Planet.cpp
+++ src/core/modules/Planet.cpp
@@ -2327,7 +2327,7 @@ float Planet::getMeanOppositionMagnitude() const
 		if (orbitPtr)
 			semimajorAxis=static_cast<KeplerOrbit*>(orbitPtr)->getSemimajorAxis();
 		else
-			qDebug() << "WARNING: No orbitPtr for " << englishName;
+			qWarning() << "WARNING: No orbitPtr for " << englishName;
 	}
 
 	if (semimajorAxis>0.)
